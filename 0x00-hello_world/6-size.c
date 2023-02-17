e524a985620c9d4d0d42748ce4ca4cd03da148dc
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-02-16 19:32:28 -0800

    my seventh commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %2d \n", sizeof(char));
+	printf("Size of an int: %2d \n", sizeof(int));
+	printf("Size of a long int: %2d \n", sizeof(long int));
+	printf("Size of long long int: %2d \n", sizeof(long long int));
+	printf("Size of a float: %2d \n", sizeof(float));
+
+	return (0);
+}
