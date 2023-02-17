81851191e51f41c5a2739d2d6a69449bb861ca91
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-02-16 17:21:38 -0800

    my fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/*this is for puts*/
+/* this is a comment*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle")
 	return (0);
 }
 
