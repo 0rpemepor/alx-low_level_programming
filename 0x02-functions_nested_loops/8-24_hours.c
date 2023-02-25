02dee03acc57420f8a28c62b46408136c32a9b50
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-02-24 18:28:45 -0800

    my ninth commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* jack_bauer - a function that prints ever minute
+*
+*
+* Return: (0) success
+*/
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);
+		}
+	}
+}
