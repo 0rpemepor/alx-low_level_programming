3df1890a9a988364c1df70fd5fa6f867e00ef185
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-02-27 15:41:22 -0800

    my test 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen - returns lenght of a string
+* @s: strng to return
+* return: the lenght of a string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
