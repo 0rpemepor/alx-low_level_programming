eadae8082112dc7f8b774f6563de7062c30264e7
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-02-28 03:09:08 -0800

    my test 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - to print every other character
+ * @str: to print strings
+ * Return: (0) Success
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		putchar(str[i]);
+		putchar('\n');
+	}
+}
