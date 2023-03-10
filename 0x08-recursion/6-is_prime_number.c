fbc3451bba4066f443d38d6a8446877c69f1cbc0
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-03-09 20:04:14 -0800

    my test 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,36 @@
 #include "main.h"
 
+int is_prime_helper(int n, int divisor);
+
 /**
- * is_prime_number - a func that returns 1 if the int is a prime num
+ * is_prime_number - Check if an integer is a prime number.
+ * @n: The integer to check.
  *
- * @n: the prime number
- *
- * Return: 1 if it's prime number otherwise 0
+ * Return: If n is a prime number, return 1. Otherwise, return 0.
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
+	if (n <= 1)  /* 1 is not a prime number */
 		return (0);
-	}
-	for (i = 2; i <= n / 2; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
+	else if (n == 2)  /* 2 is a prime number */
+		return (1);
+	else
+		return (is_prime_helper(n, 2));
+}
 
-	return (1);
+/**
+ * is_prime_helper - Check if an integer is a prime number.
+ * @n: The integer to check.
+ * @divisor: The current divisor to check.
+ *
+ * Return: If n is a prime number, return 1. Otherwise, return 0.
+ */
+int is_prime_helper(int n, int divisor)
+{
+	if (n % divisor == 0)  /* n is not a prime number */
+		return (0);
+	else if (divisor * divisor > n)  /* n is a prime number */
+		return (1);
+	else
+		return (is_prime_helper(n, divisor + 1));
 }
