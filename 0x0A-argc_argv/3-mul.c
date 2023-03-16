aa95ad747bb0681bc2975f1ceaf8f0e9daf7ce58
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-03-15 21:08:45 -0700

    my test 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * multiplication - a program that multiplies two args
+ * @a: first args
+ * @b: second args
+ * Return: a and b
+ */
+int multiplication(int a, int b)
+{
+	return a * b;
+}
+
+/**
+ * main - it calls the multiplication function
+ * @argc: it counts arguments
+ * @argv: it holds arguments
+ * Return: 0 always
+ */
+int main (int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+		printf("Usage: %s <integer1> <integer2>\n", argv[0]);
+		return 1;
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	result = multiplication(a, b);
+
+	printf("The multiplication of %d and %d is: %d\n", a, b, result);
+
+	return 0;
+}
