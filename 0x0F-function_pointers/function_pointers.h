e16e51d3daa0d0c3f97e6713b7057b4dc1f0a214
0rpemepor <ibrahimolasunkanmi98@gmail.com>
2023-03-23 03:22:07 -0700

    The Header File

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FILE_FUNCTION
 #define FILE_FUNCTION
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
